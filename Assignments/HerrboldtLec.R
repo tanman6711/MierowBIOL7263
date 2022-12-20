07ef86a7de3b2f952593222b6da7e22be78bcd4c
Tanner Mierow <tannermierow@Tanners-MacBook-Pro.local>
2022-12-19 21:30:06 -0600

    fixing hw

--- Assignments/HerrboldtLec.R
+++ Assignments/HerrboldtLec.R
@@ -1,30 +1,14 @@
-heatmap(pheromone_data1, 
-        Colv = NA, 
-        Rowv = NA, 
-        margins = c(7, 5), 
-        cexcol = 1,
-        col = terrain.colors(10),
-        main = "Pheromone Gene Expression", 
-        xlab = "Groups", 
-        ylab = "Genes")
+# Create data variable
+AssignmentData<- MAH_assignment_data
 
-pheromone_data2 <- read.csv("pheromone_data2.csv")
-
-ggplot(pheromone_data2,
+# Create GG plot with assignment data
+ggplot(AssignmentData,
        aes(x = Sample, y = Gene, fill = Expression)) +
 geom_tile()
 
-ggplot(pheromone_data2,
-       aes(x = Sample, y = Gene, fill = Expression)) +
-  geom_tile(colour = "black", linewidth = 0.5) +
-  scale_fill_gradient(low = "white", high = "purple") +
-  theme_grey(base_size = 12) +
-  theme(axis.ticks = element_blank(),
-        plot.background = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_blank())
+# Create heat map!
 
-ggplot(pheromone_data2,
+ggplot(AssignmentData,
        aes(x = Sample, y = Gene, fill = Expression)) +
   geom_tile(colour = "black", linewidth = 0.5) +
   scale_fill_gradient(low = "white", high = "purple") +
